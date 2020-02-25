86005acb1d58c8d70d5260e3610547b65d51f63f
jrrj67 <ricardojunior.6767@gmail.com>
2020-02-25 03:30:39 -0300

    Alterando rota no logo

--- materialize/templates/main.php
+++ materialize/templates/main.php
@@ -1,7 +1,7 @@
 <main>
     <nav class="blue">
         <div class="nav-wrapper">
-            <a href="#!" class="center brand-logo">Bin2Dec</a>
+            <a href="<?php $_SERVER['REQUEST_URI'] ?>" class="center brand-logo">Bin2Dec</a>
             <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
             <ul class="right hide-on-med-and-down">
                 <li><a href="https://github.com/jrrj67">My Github</a></li>
